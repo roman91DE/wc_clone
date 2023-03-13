edafff87342cae423e68742bbe8b5518c69a483d
rmn@mbp <rohoehn123@gmail.com>
2023-03-13 19:54:45 +0100

    Initial Commit

---
+++ wc_clone/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  wc_clone
+//
+//  Created by roman on 13.03.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
